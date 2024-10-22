3aa83bc80670c77ade6dcf217906d0eeffdf8b75
Richard-WYZ <905404456@qq.com>
2024-10-23 08:57:02 +1000

    add some new pages and PWA

---
+++ assessment/js/recipeDetails.js
@@ -0,0 +1,28 @@
+import { fetchRecipes } from "./components.js";
+
+// get specific parameters in the url
+const urlParams = new URLSearchParams(window.location.search);
+const recipeName = urlParams.get("name");
+const createdAt = urlParams.get("created_at");
+
+// Function to display recipes content
+function displayRecipeContent(recipes) {
+  const recipeDetail = document.querySelector(".detailed-information");
+  // find current recipe by name and created time
+  const cur_recipe = recipes.find(
+    (r) => r.recipe_name === recipeName && r.created_at === createdAt
+  );
+
+  if (cur_recipe) {
+    // exist
+  } else {
+    console.error("Recipe not found");
+    recipeDetail.innerHTML = "Recipe not found.";
+  }
+}
+
+function handleGETError(Error) {
+  console.log("Error", Error);
+}
+
+fetchRecipes(displayRecipeContent, handleGETError);
--- assessment/js/recipes.js
+++ assessment/js/recipes.js
@@ -2,19 +2,28 @@ import { fetchRecipes } from "./components.js";
 
 // Function to display recipes as cards
 function displayRecipes(recipes) {
-  const recipeList = document.getElementById("recipe-list");
+  const recipeList = document.querySelector(".recipe-list");
   recipeList.innerHTML = "";
 
   recipes.forEach((recipe) => {
     const recipeCard = document.createElement("div");
     recipeCard.className = "recipe-card"; // card component
+
+    /*
+    use both recipe name and created time to ensure the recipe is unique
+    encode to avoid special characters like space or question mark
+    */
     recipeCard.innerHTML = `
-          <h3>${recipe.recipe_name}</h3>
-          <img src="${recipe.image}" alt="${recipe.recipe_name}" />
-          <p><strong>Author:</strong> ${recipe.author}</p>
-          <p><strong>Cooking Time:</strong> ${recipe.cooking_time} minutes</p>
-          <p><strong>Servings:</strong> ${recipe.servings}</p>
-          <p><strong>Difficulty:</strong> ${recipe.difficulty_level}</p>
+          <a href="./recipeDetails.html?name=${encodeURIComponent(
+            recipe.recipe_name
+          )}&created_at=${encodeURIComponent(recipe.created_at)}">
+            <h3>${recipe.recipe_name}</h3>
+            <img src="${recipe.image}" alt="${recipe.recipe_name}" />
+            <p><strong>Author:</strong> ${recipe.author}</p>
+            <p><strong>Cooking Time:</strong> ${recipe.cooking_time} minutes</p>
+            <p><strong>Servings:</strong> ${recipe.servings}</p>
+            <p><strong>Difficulty:</strong> ${recipe.difficulty_level}</p>
+          </a>
       `;
     recipeList.appendChild(recipeCard);
   });
